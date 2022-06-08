cf4fc5a831792454d5447344bdd30a67267249c1
MrMelon <github@onpointcoding.net>
2022-06-09 00:10:19 +0100

    Migrate to 1.19

--- src/main/java/xyz/mrmelon54/BedrockDestroyer/mixin/MixinStorageMinecartEntity.java
+++ src/main/java/xyz/mrmelon54/BedrockDestroyer/mixin/MixinStorageMinecartEntity.java
@@ -21,7 +21,7 @@ public abstract class MixinStorageMinecartEntity implements IMinecartEntitySize 
     @Shadow
     public abstract void setStack(int slot, ItemStack stack);
 
-    @Inject(method = "interact", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;openHandledScreen(Lnet/minecraft/screen/NamedScreenHandlerFactory;)Ljava/util/OptionalInt;"))
+    @Inject(method = "interact", at = @At("HEAD"))
     public void wrapInteract(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
         if (BedrockDestroyer.getInstance().shouldIgnorePlayer(player)) return;
 
